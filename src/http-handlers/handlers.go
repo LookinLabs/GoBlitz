1d8981965908624e5ef4a1dfc28e4071ae51b426
KostLinux <christofher.kost@lhv.ee>
2024-04-03 11:48:13 +0300

    Added 502 error handler

--- src/http-handlers/handlers.go
+++ src/http-handlers/handlers.go
@@ -9,6 +9,19 @@ func notFoundHandler(c *gin.Context) {
 	c.File("./public/error/404.html")
 }
 
+func serverErrorHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if request := recover(); request != nil {
+				c.Status(502)
+				c.File("./public/error/502.html")
+			}
+		}()
+		c.Next()
+	}
+}
+
 func Handlers(router *gin.Engine) {
+	router.Use(serverErrorHandler())
 	router.NoRoute(notFoundHandler)
 }
