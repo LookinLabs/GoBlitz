995c30030768be0ea1fb8081119ef6320a492f12
KostLinux <githubchristofher@gmail.com>
2024-06-02 09:41:26 +0300

    Removed duplicate code

--- helpers/file.go
+++ helpers/file.go
@@ -6,8 +6,3 @@ func CheckFileExists(path string) bool {
 	_, err := os.Stat(path)
 	return !os.IsNotExist(err)
 }
-
-func CheckFileNotExists(path string) bool {
-	_, err := os.Stat(path)
-	return os.IsNotExist(err)
-}
