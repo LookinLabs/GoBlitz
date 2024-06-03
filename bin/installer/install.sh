22916fd98880eee93f9b7c03aa0151763ae459e8
KostLinux <githubchristofher@gmail.com>
2024-06-03 08:32:28 +0300

    Separated install and added update

--- bin/installer/install.sh
+++ bin/installer/install.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-source src/*.sh
+for i in $(pwd)/bin/installer/src/*.sh; do
+    source $i
+done
 
 OS=$(uname -s)
-VERSION=$1
+OPTION=$1
+VERSION=$2
 
 if [[ -z $VERSION ]]; then
-    echo "No version specified. Please specify the version to install."
+    echo "No version specified. Please specify the version to install or update."
     echo "Available versions can be tag or branch name. Example: v1.0.0 or master"
     exit 1
 fi
 
-if [[ $OS == "Darwin" ]]; then
-    install_on_macos
-    install_goblitz
-fi
+case $OPTION in
+    --install)
+        if [[ $OS == "Darwin" ]]; then
+            install_macos_utilities $VERSION
+            install_goblitz $VERSION
+        fi
+        ;;
+    --update)
+        if [[ $OS == "Darwin" ]]; then
+            update_goblitz $VERSION
+        fi
+        ;;
+    *)
+        echo "Invalid option. Please use --install or --update."
+        exit 1
+        ;;
+esac
--- bin/installer/src/common.sh
+++ bin/installer/src/common.sh
@@ -3,20 +3,21 @@
 function install_goblitz() {
     if ! [ -d "$(pwd)/GoBlitz" ]; then
         echo "GoBlitz directory does not exist. Pulling GoBlitz..."
-        git clone git@github.com:LookinLabs/GoBlitz.git --depth 1 --branch $VERSION
+        git config --global advice.detachedHead false
+        git clone git@github.com:LookinLabs/GoBlitz.git --branch $VERSION
+        rm -rf GoBlitz/.git
     fi
 }
 
 function update_goblitz() {
     if `pwd | grep -q "GoBlitz"` && [ "$VERSION" == "master" ]; then
         echo "GoBlitz directory found. Pulling updates..."
-        git pull origin master
+        git merge origin/master
     fi
     
     if `pwd | grep -q "GoBlitz"` && [ "$VERSION" != "master" ]; then
         echo "GoBlitz directory found. Checking out $VERSION..."
         git fetch
-        git checkout $VERSION
-        git pull
+        git merge $VERSION
     fi
 }
